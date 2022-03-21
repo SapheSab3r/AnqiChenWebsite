12e50cd1daa38d5014ead268b36c5d1bc5f1e31b
Anqi <anqi24.com@gmail.com>
2022-03-21 01:26:27 -0400

    adding features

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-// When the user scrolls the page, execute navFunction
-window.onscroll = function() {navFunction()};
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".menu");
 
-// Get the navbar
-var navbar = document.getElementsByClassName("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function navFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+    });
 }
+
+navSlide();
