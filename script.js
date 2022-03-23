2a36a662bcf0bf04f65dedce293e807bb0ef7543
Anqi <anqi24.com@gmail.com>
2022-03-23 00:14:37 -0400

    last change

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// slide when small screen
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".menu");
@@ -8,3 +9,18 @@ const navSlide = () => {
 }
 
 navSlide();
+
+// sticky navigation bar
+window.onscroll = function (){fixedNav()};
+
+const navbar = document.querySelector(".navbar");
+const sticky = navbar.offsetTop;
+
+function fixedNav() {
+    if (window.pageYOffset > sticky){
+        navbar.classList.add("sticky")
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
