4b08482e3546778c74a32d20779720052d39a805
Anqi <anqi24.com@gmail.com>
2022-03-27 01:46:07 -0400

    addition

--- script.js
+++ script.js
@@ -24,3 +24,16 @@ function fixedNav() {
         navbar.classList.remove("sticky");
     }
 }
+
+
+// to-top buttom
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+} )
