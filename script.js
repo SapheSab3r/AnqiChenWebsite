6f1d2fe25b2523576d0aef0cd469ade2eebf47c0
Anqi <anqi24.com@gmail.com>
2022-04-09 01:44:00 -0400

    n

--- script.js
+++ script.js
@@ -87,15 +87,22 @@ function sendEmail(){
 const product = document.querySelector(".product");
 const popup = document.querySelector(".popup-box");
 const popupCloseBtn = popup.querySelector(".popup-close-btn");
+const popupCloseIcon = popup.querySelector(".popup-close-icon");
 
 product.addEventListener("click",function(event){
     if(event.target.tagName.toLowerCase() == "button"){
+        const item = event.target.parentElement;
+        const h2 = item.querySelector("h2").innerHTML;
+        const readMoreCont = item.querySelector(".read-more-cont").innerHTML;
+        popup.querySelector("h2").innerHTML = h2;
+        popup.querySelector(".popup-body").innerHTML = readMoreCont;
         popupBox();
-
     }
 })
 
 popupCloseBtn.addEventListener("click", popupBox);
+popupCloseIcon.addEventListener("click", popupBox);
+
 popup.addEventListener("click", function(event){
     if(event.target == popup){
        popupBox();
