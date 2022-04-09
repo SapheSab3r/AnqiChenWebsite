8c8fc86992f5d4100b6bc55f71a140e351452196
Anqi <anqi24.com@gmail.com>
2022-04-09 16:24:10 -0400

    add

--- script.js
+++ script.js
@@ -113,3 +113,26 @@ popup.addEventListener("click", function(event){
 function popupBox(){
     popup.classList.toggle("open");
 }
+
+// scroll Animation
+const fader = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+    threshold: 0.3,
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry =>{
+        if (!entry.isIntersecting){
+            return;
+        }
+        else{
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+fader.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
